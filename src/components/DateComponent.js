f3615da4a0804db16710d76c23f6e1298cbd60f3
ROSMIGEORGE <rosmigeorge@outlook.com>
2020-04-01 14:58:09 +0530

    date component fix

--- src/components/DateComponent.js
+++ src/components/DateComponent.js
@@ -10,8 +10,7 @@ class DateComponent extends Component {
             month: this.props.dateSelected.getMonth(),
             year: this.props.dateSelected.getFullYear(),
             dayList: [],
-            showCalender: this.props.dateSelected,
-            dateSelected: this.props.dateSelected
+            showCalender: this.props.dateSelected
         }
     }
 
@@ -30,7 +29,11 @@ class DateComponent extends Component {
             mm: InputDate.getMonth(),
             yy: InputDate.getFullYear()
         };
-        const dateSelected = this.props.dateSelected;
+        const s_date = {
+            dd: this.props.dateSelected.getDate(),
+            mm: this.props.dateSelected.getMonth(),
+            yy: this.props.dateSelected.getFullYear()
+        };
 
         for (let i = 0; i < c_date.day; i++) {
             tag = <div key={key} className="disabled-btn"></div>
@@ -40,22 +43,23 @@ class DateComponent extends Component {
 
         while (currentDate.getMonth() === c_date.mm) {
             day = currentDate.getDate();
-            if (dateSelected.getDate() === day && dateSelected.getMonth() === c_date.mm && dateSelected.getFullYear() === c_date.yy) {
-                className = 'day-btn selected';
-            }
-            else {
-                className = 'day-btn';
-            }
             if (currentDate > TODAY) {
                 tag = <div key={key} className="disabled-btn" day={day} >{day}</div>
             }
             else {
+                if (s_date.dd === day && s_date.mm === c_date.mm && s_date.yy === c_date.yy) {
+                    className = 'day-btn selected';
+                }
+                else {
+                    className = 'day-btn';
+                }
                 tag = <div key={key} className={className} day={day} onClick={this.updateDate}>{day}</div>
             }
             dayList.push(tag);
             key++;
             currentDate.setDate(currentDate.getDate() + 1);
         }
+        currentDate.setDate(currentDate.getDate() - 1);
         this.setState({
             dayList: dayList
         });
@@ -128,7 +132,7 @@ class DateComponent extends Component {
     }
 
     render() {
-        const { date, month, year, dayList, showCalender, dateSelected } = this.state;
+        const { date, month, year, dayList, showCalender } = this.state;
         const monthList = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
         return (
             <div className="calender-wrapper">
